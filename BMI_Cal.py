60e205900e29f035954bcd8bbc2d2e9141859909
Priyanshu Kumar Bhagat <149898239+priyanshucodes73@users.noreply.github.com>
2024-05-11 01:23:22 +0530

    Add files via upload

---
+++ BMI_Cal.py
@@ -0,0 +1,9 @@
+# Python
+height = input("Upload Height")
+weight = input("Upload Height")
+weight_as_int = int(weight)
+height_as_float = float(height)
+bmi = weight_as_int/height_as_float ** 2
+# BMI = Body Mass Index
+bmi_as_int = int(bmi)
+print(bmi)
